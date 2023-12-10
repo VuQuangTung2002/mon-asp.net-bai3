cade13fcf94a5a948f442d381acbdee238a5812e
VuQUangTung2002 <vutung1234567890@gmail.com>
2023-12-10 15:51:23 +0700

    bai 3.1 asp.net

---
+++ Controllers/ThongTinSinhVienController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bai3.Controllers
+{
+    public class ThongTinSinhVienController : Controller
+    {
+        // GET: ThongTinSinhVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult View2()
+        {
+            ViewBag.Message= "Thông tin chi tiết sinh viên";
+            return View();
+        }
+    }
+}
